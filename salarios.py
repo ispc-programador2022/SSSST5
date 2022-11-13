9e797bb85e2aa736c5cde1eaa6fdcba06731d00b
JorgelinaTissera <106881754+JorgelinaT@users.noreply.github.com>
2022-11-12 22:16:52 -0300

    Update salarios.py
    Código mejorado usando funciones para optimizar. Análisis de los sueldos contenidos en el archivo escala_salarial.csv, obtención del archivo dictSalarios.csv con toda la información pertinente

--- salarios.py
+++ salarios.py
@@ -1,4 +1,14 @@
 
+import csv
+
+def promedios(suma, cantidad):
+    promedios=suma//cantidad
+    return promedios
+
+def ahorro (promedio):
+    canasta=128214
+    return (promedio - canasta)
+
 
 separador=","
 with open('escala_salarial.csv', encoding="utf-8") as archivo:
@@ -8,16 +18,16 @@ with open('escala_salarial.csv', encoding="utf-8") as archivo:
         linea=linea.rstrip("\n")  #quito el salto de línea
         columnas=linea.split(separador)
         cargo=columnas[0]
-        liquidacion=float(columnas[1])
-        
+
+
+        liquidacion=int(columnas[1])
         
         Salarios.append({
             "cargo":cargo,
             "liquidacion":liquidacion,  
             
         })
-   # print("\n\nArchivo CSV convertido a Diccionario:\n\n")
-#print(Salarios) 
+   
 #Análisis de intervalos de los sueldos:
 cant_menor_igual_150=0
 cant_150_y_300=0
@@ -27,19 +37,21 @@ suma1=0
 suma2=0
 suma3=0
 suma4=0
+
 for salario in Salarios:
     val_salario = salario["liquidacion"]
-    if val_salario <= 150.000:
+
+    if val_salario <= 150000:
         suma1+=val_salario
         cant_menor_igual_150+=1
         
         
-    elif val_salario > 150.000 and val_salario <= 300.000:
+    elif val_salario > 150000 and val_salario <= 300000:
         cant_150_y_300+=1
         suma2+=val_salario
         
 
-    elif val_salario > 300.000 and val_salario <= 500.000:
+    elif val_salario > 300000 and val_salario <= 500000:
         cant_300_y_500+=1
         suma3+=val_salario
         
@@ -47,17 +59,57 @@ for salario in Salarios:
         cant_mayor_500+=1
         suma4+=val_salario
         
+total_sueldos=cant_menor_igual_150+cant_150_y_300+cant_300_y_500+cant_mayor_500
+print("\n LA CANTIDAD TOTAL DE SALARIOS ANALIZADOS ES: ", total_sueldos,"\n")
 
 
-
+#Intervalos sueldos menores a $150000
 print ("\nLa cantidad de sueldos menores a 150000 son: ",cant_menor_igual_150)
-print("\nEl sueldo promedio entres los sueldos menores o iguales a $150000 es: $",round(suma1/cant_menor_igual_150,3))
+
+print("\nEl sueldo promedio entres los sueldos menores o iguales a $150000 es: $",promedios(suma1, cant_menor_igual_150), "por lo que su capacidad de ahorro es nula \n, ya que no cubre el monto de la canasta básica ($128.214)")
+
+#Intervalos sueldos comprendidos entre 150000 y 300000
 print("\nLa cantidad de sueldos mayores a 150000 y menores o iguales a 300000 son: ",cant_150_y_300)
-print("\nEl sueldo promedio entres los sueldos entres $150000 y $300000 es: $",round(suma2/cant_150_y_300,3))
+
+prom_150_300=promedios(suma2, cant_150_y_300)
+print("\nEl sueldo promedio entres los sueldos entres $150000 y $300000 es: $",prom_150_300)
+print("\nLa capacidad de ahorro mensual de este grupo es: $",ahorro(prom_150_300))
+
+#Intervalos sueldos comprendidos entre 300000 y 500000
 print("\nLa cantidad de sueldos mayores a 300000 y menores o iguales a 500000 son: ",cant_300_y_500)
-print("\nEl sueldo promedio entres los sueldos mayores a $300000 y menores a 500000 es: $",round(suma3/cant_300_y_500,3))
+
+prom_300_500=promedios(suma3,cant_300_y_500)
+print("\nEl sueldo promedio entres los sueldos mayores a $300000 y menores a 500000 es: $",prom_300_500)
+print("\nLa capacidad de ahorro mensual de este grupo es: $",ahorro(prom_300_500))
+
+#Intervalos sueldos mayores a 500000
 print("\nLa cantidad de sueldos mayores a 500000 son: ",cant_mayor_500)   
-print("\nEl sueldo promedio entre los sueldos mayores a $500000 es: $", suma4/cant_mayor_500)
-print("\n La cantidad total salarios analizados es: ", cant_menor_igual_150+cant_150_y_300+cant_300_y_500+cant_mayor_500,"\n")
+prom_500=promedios(suma4, cant_mayor_500) 
+print("\nEl sueldo promedio entre los sueldos mayores a $500000 es: $", prom_500)
+print("\nLa capacidad de ahorro de este grupo es: $",ahorro(prom_500))
+
+
+dictSalarios={'sueldos':total_sueldos,
+                 "sueldos_menores_150000":cant_menor_igual_150,
+                 "sueldo_promedio_150000":promedios(suma1, cant_menor_igual_150),
+                 "sueldos_150_300":cant_150_y_300,
+                 "sueldos_promedio_150_300":prom_150_300,
+                 "ahorros_150_300":ahorro(prom_150_300),
+                 "sueldos_300_500":cant_300_y_500,
+                 "sueldos_promedio_300_500":prom_300_500,
+                 "ahorros_300_500":ahorro(prom_300_500),
+                 "sueldos_500":cant_mayor_500,
+                 "sueldos_promedio_500":prom_500,
+                 "ahorros_500":ahorro(prom_500)
+                 }
+
+
+#print(dictSalarios) 
+
+#Creación del archivo CSV con los datos del análisis de los sueldos 
+with open('dictSalarios.csv','w')as f:
+    writer=csv.writer(f)
+    for k,v in dictSalarios.items():
+        writer.writerow([k,v])
 
 
